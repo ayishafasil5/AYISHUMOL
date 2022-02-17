737fb3422a5f1b6cc236f2769c3ce34f5e54fce9
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-02-17 08:21:56 +0530

    Update conventer.js

--- plugins/conventer.js
+++ plugins/conventer.js
@@ -1,6 +1,6 @@
 /*Codded by @phaticusthiccy
-Telegram: https://t.me/phaticusthiccy
-Instagram: https://instagram.com/kyrie.baran
+recodded by afnanplk
+PINKY V2 
 */
 
 const Asena = require('../events');
@@ -120,7 +120,7 @@ if (Config.WORKTYPE == 'private') {
             await Axios({ method: "GET", url: rest.result, responseType: "stream"}).then(({ data }) => {
                 const saving = data.pipe(fs.createWriteStream('/root/WhatsAsenaDuplicated/stweb.mp4'))
                 saving.on("finish", async () => {
-                    await message.client.sendMessage(mid, fs.readFileSync('/root/WhatsAsenaDuplicated/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: '𝐌𝐚𝐝𝐞 𝐁𝐲 𝐌𝐢𝐬𝐬 𝐇𝐞𝐥𝐞𝐧𝐚', quoted: message.data })
+                    await message.client.sendMessage(mid, fs.readFileSync('/root/WhatsAsenaDuplicated/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: 'Made by PINKY', quoted: message.data })
                     if (fs.existsSync(savedFilename)) fs.unlinkSync(savedFilename)
                     if (fs.existsSync('/root/WhatsAsenaDuplicated/stweb.mp4')) fs.unlinkSync('/root/WhatsAsenaDuplicated/stweb.mp4')
                 })
@@ -185,51 +185,12 @@ else if (Config.WORKTYPE == 'public') {
             await Axios({ method: "GET", url: rest.result, responseType: "stream"}).then(({ data }) => {
                 const saving = data.pipe(fs.createWriteStream('/root/WhatsAsenaDuplicated/stweb.mp4'))
                 saving.on("finish", async () => {
-                    await message.client.sendMessage(mid, fs.readFileSync('/root/WhatsAsenaDuplicated/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: '𝐌𝐚𝐝𝐞 𝐁𝐲 𝐌𝐢𝐬𝐬 𝐇𝐞𝐥𝐞𝐧𝐚', quoted: message.data })
+                    await message.client.sendMessage(mid, fs.readFileSync('/root/WhatsAsenaDuplicated/stweb.mp4'), MessageType.video, { mimetype: Mimetype.mp4, caption: 'inna nanba', quoted: message.data })
                     if (fs.existsSync(savedFilename)) fs.unlinkSync(savedFilename)
                     if (fs.existsSync('/root/WhatsAsenaDuplicated/stweb.mp4')) fs.unlinkSync('/root/WhatsAsenaDuplicated/stweb.mp4')
                 })
             })
         })
     }));
-    
-    var doc_desc = ''
-    var plk = ''
-    var afn = ''
-    var usge = ''
-     if (Config.LANG == 'EN') {
-        doc_desc = 'CONVERT TO DOCUMENT AND ADD GIVEN NAME'
-        plk = '```NAMING AND DOCIFYING```'
-        afn = '```PLEASE REPLY TO A AUDIO```'
-        usge = '```.doc Helena *replace helena with desired name*```'
-    }
-    if (Config.LANG == 'ML') {
-        doc_desc = 'ഡോക്യുമെന്റിലേക്ക് പരിവർത്തനം ചെയ്യുകയും നൽകിയപേര് ചേർക്കുകയും ചെയ്യുക'
-        afn = '```ഒരു ഓഡിയോയ്ക്ക് മറുപടി നൽകുക```'
-        plk = '```ഡോക്യുമെന്റിലേക്ക് പരിവർത്തനം ചെയ്യുകയും പേര് നൽകുകയും ചെയ്യുന്നു```'
-        usge = '```.doc Helena```'
-    }
-    
-     Asena.addCommand({pattern: 'doc ?(.*)', fromMe: false, desc: doc_desc , usage : usge}, (async (message, match) => { 
-      
-        if (match[1] === '') return await message.client.sendMessage(message.jid,'give me a name',MessageType.text);  
-        const mid = message.jid
-        if (message.reply_message === false) return await message.client.sendMessage(mid,afn, MessageType.text);
-        var downloading = await message.client.sendMessage(mid,plk,MessageType.text);
-        var location = await message.client.downloadAndSaveMediaMessage({
-            key: {
-                remoteJid: message.reply_message.jid,
-                id: message.reply_message.id
-            },
-            message: message.reply_message.data.quotedMessage
-        });
-
-        ffmpeg(location)    
-            .save('output.mp3')
-            .on('end', async () => {
-                await message.client.sendMessage(mid, fs.readFileSync('output.mp3'), MessageType.document, {filename: match[1] + '.mp3', mimetype: 'audio/mpeg', quoted: message.data});
-            });
-        return await message.client.deleteMessage(mid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
-    }));
-
 }
+    
